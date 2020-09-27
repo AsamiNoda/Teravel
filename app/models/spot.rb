defda737cf423977753f5b95658babf3010a5829
vagrant <vagrant@localhost.localdomain>
2020-09-27 10:53:37 +0000

    [modified]map

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,21 +1,21 @@
 class Spot < ApplicationRecord
   belongs_to :post
   geocoded_by :address
-  after_validation :geocode
+  before_validation :geocode
   validates :address ,presence: true
 
- #  validate :check_latitude
- #  validate :check_longitude
+  validate :check_latitude
+  validate :check_longitude
 
- # def check_latitude
-	# if latitude.nil?
-	# 	errors.add(:latitude,  "の値が不正です")
-	# end
- #  end
- #  def check_longitude
- #  	if longitude.nil?
- #  		errors.add(:longitude,  "の値が不正です")
- #  	end
- #  end
+ def check_latitude
+	if self.latitude.nil?
+		errors.add(:latitude,  "の値が不正です")
+	end
+  end
+  def check_longitude
+  	if self.longitude.nil?
+  		errors.add(:longitude,  "の値が不正です")
+  	end
+  end
 
 end
