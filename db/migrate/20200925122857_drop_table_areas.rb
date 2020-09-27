864bd33f4d1c2a7099f996d3f0068e09263adbae
vagrant <vagrant@localhost.localdomain>
2020-09-27 05:33:34 +0000

    [delete]db file

--- db/migrate/20200925122857_drop_table_areas.rb
+++
@@ -1,9 +0,0 @@
-class DropTableAreas < ActiveRecord::Migration[5.2]
-  def change
-	  	drop_table :areas do |t|
-	    t.string "name"
-	    t.datetime "created_at", null: false
-	    t.datetime "updated_at", null: false
-	  end
-  end
-end
